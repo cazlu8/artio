0edf0f58c2a8bd3868183f6574e0a4f197426149
cazlu8 <cazlubios8@gmail.com>
2020-04-29 20:35:35 -0300

    removed google logging.

--- src/shared/services/logger.service.ts
+++ src/shared/services/logger.service.ts
@@ -1,4 +1,3 @@
-import { Log, Logging } from '@google-cloud/logging';
 import { Injectable } from '@nestjs/common';
 // passar p aws logging
 @Injectable()
@@ -7,24 +6,13 @@ export class LoggerService {
 
   private logName: string;
 
-  private logger: Log;
-
   private metadata: {};
 
   constructor() {
-    this.projectId = process.env.GOOGLE_CLOUD_PROJECT_ID;
-    this.logName = process.env.GOOGLE_CLOUD_LOG_NAME;
-    const logging = new Logging({ projectId: this.projectId });
-    this.logger = logging.log(this.logName);
-    this.metadata = {
-      resource: { type: 'global' },
-    };
+
   }
 
   log(text: string) {
-    if (process.env.NODE_ENV === 'test') return;
-    const entry = this.logger.entry(this.metadata, text);
-    this.logger.write(entry).catch(console.log);
-    console.log(`Logged: ${text}`);
+
   }
 }
