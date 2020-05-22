0df758abc844e3ec2cd3f1eac32432bebf96b8ba
Pedro Diniz <pedro_nork@hotmail.com>
2020-05-22 19:40:41 -0300

    Fix validate token call (#39)
    * - fix validateToken call
    
    * - creation of validate token method

--- src/shared/middlewares/auth.middleware.ts
+++ src/shared/middlewares/auth.middleware.ts
@@ -15,19 +15,20 @@ export class AuthMiddleware implements NestMiddleware {
     try {
       const { authorization } = req.headers;
       const token = authorization.slice(7);
-      req.user = await any([...validateToken(token)]);
+      req.user = await any([...this.validateToken(token)]);
       next();
     } catch (error) {
       throw new UnauthorizedException();
     }
-    function validateToken(token: string) {
-      return [
-        process.env.JWT_SECRET_KEY_LOCAL,
-        process.env.JWT_SECRET_KEY_STAGING,
-      ].map(async key => {
-        const publicKey = key.replace(/\\n/g, '\n');
-        await verifyToken(token, publicKey);
-      });
-    }
+  }
+
+  private validateToken(token: string) {
+    return [
+      process.env.JWT_SECRET_KEY_LOCAL,
+      process.env.JWT_SECRET_KEY_STAGING,
+    ].map(async key => {
+      const publicKey = key.replace(/\\n/g, '\n');
+      await verifyToken(token, publicKey);
+    });
   }
 }
