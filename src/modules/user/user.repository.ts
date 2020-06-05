254c846c191b3cace6e3f93019ea4c7039200363
PedroDiniz <pedro_nork@hotmail.com>
2020-06-05 14:58:02 -0300

    - moving delete from repository to service

--- src/modules/user/user.repository.ts
+++ src/modules/user/user.repository.ts
@@ -1,8 +1,5 @@
 import { EntityRepository, Repository } from 'typeorm';
-import * as AWS from 'aws-sdk';
-import { InternalServerErrorException } from '@nestjs/common';
 import { User } from './user.entity';
-import { s3Config } from '../../shared/config/AWS';
 
 @EntityRepository(User)
 export class UserRepository extends Repository<User> {
@@ -12,19 +9,5 @@ export class UserRepository extends Repository<User> {
 
   async get({ where, select }) {
     return this.findOne({ select, where });
-  }
-
-  async deleteAvatar(user: User, Bucket: string): Promise<void> {
-    const s3 = new AWS.S3(s3Config());
-    if (user?.avatarImgUrl) {
-      try {
-        const { avatarImgUrl: formerUrl } = user;
-        const lastIndex = formerUrl.lastIndexOf('/');
-        const currentKey = formerUrl.substr(lastIndex + 1, formerUrl.length);
-        await s3.deleteObject({ Bucket, Key: `${currentKey}.png` }).promise();
-      } catch (error) {
-        throw new InternalServerErrorException(error);
-      }
-    }
   }
 }
