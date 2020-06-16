67e72f30cf9b4918247b5178b63b00cc6dbb9ac1
Pedro Diniz <pedro_nork@hotmail.com>
2020-06-16 19:01:22 -0300

    Refactor Twilio (#76)
    * - initial structure of twilio
    
    * - typing token
    
    * - typing token
    
    * - typing token
    
    * - fix on create room
    
    * - fix on create room
    
    * - fix on create room
    
    * - refactor on networkRoom.service.ts

--- src/modules/networkRoom/networkRoom.service.ts
+++ src/modules/networkRoom/networkRoom.service.ts
@@ -3,39 +3,48 @@ import { InjectRepository } from '@nestjs/typeorm';
 import { ObjectLiteral, Repository } from 'typeorm';
 import { uuid } from 'uuidv4';
 import * as twilio from 'twilio';
-import { twilioConfig, clientConfig } from '../../shared/config/Twilio';
+import { config } from '../../shared/config/Twilio';
 import { NetworkRoom } from './networkRoom.entity';
 import { NetworkRoomTokenDto } from './dto/networkRoomToken.dto';
 
 const { AccessToken } = twilio.jwt;
 const { VideoGrant } = AccessToken;
+const catchError = err => {
+  throw new Error(err);
+};
 
 @Injectable()
 export class NetworkRoomService {
+  private readonly clientConfig: any;
+
+  private readonly twilioConfig: any;
+
   constructor(
     @InjectRepository(NetworkRoom)
     private readonly repository: Repository<NetworkRoom>,
-  ) {}
+  ) {
+    const { clientConfig, twilioConfig } = config();
+    this.clientConfig = clientConfig();
+    this.twilioConfig = twilioConfig();
+  }
 
   async createRoom(): Promise<any> {
     const uid = uuid();
-    return await clientConfig()
-      .video.rooms.create({
+    return await this.clientConfig.video.rooms
+      .create({
         recordParticipantsOnConnect: true,
         type: 'group-small',
         uniqueName: uid,
       })
-      .then(room => {
-        return { sid: room.sid, uniqueName: room.uniqueName };
-      })
-      .catch(console.log);
+      .then(room => ({ sid: room.sid, uniqueName: room.uniqueName }))
+      .catch(catchError);
   }
 
   getRoom(rooms): Promise<any> {
     const roomUniqueName = rooms.uniqueName;
     const roomSid = rooms.sid;
-    return clientConfig()
-      .video.rooms(roomSid)
+    return this.clientConfig.video
+      .rooms(roomSid)
       .participants.list(
         { status: 'connected' },
         async (error, participants) => {
@@ -45,49 +54,33 @@ export class NetworkRoomService {
           return await this.createRoom();
         },
       )
-      .catch(console.log);
+      .catch(catchError);
   }
 
-  room(): Promise<ObjectLiteral | void> {
-    return clientConfig()
-      .video.rooms.list({ status: 'in-progress' })
-      .then(async rooms => {
-        const availableRoom = rooms.find(
-          async ({ sid, uniqueName }) =>
-            await this.getRoom({ sid, uniqueName }),
-        );
-        if (availableRoom) {
-          return {
-            uniqueName: availableRoom.uniqueName,
-            sid: availableRoom.sid,
-          };
-        }
-        return await this.createRoom();
-      })
-      .catch(console.log);
+  async room(): Promise<ObjectLiteral | void> {
+    const rooms = await this.clientConfig.video.rooms.list({
+      status: 'in-progress',
+    });
+    const availableRoom = rooms.find(
+      async ({ sid, uniqueName }) => await this.getRoom({ sid, uniqueName }),
+    );
+    return availableRoom || (await this.createRoom());
   }
 
   videoToken(networkRoomTokenDto: NetworkRoomTokenDto): ObjectLiteral {
     const { identity, room } = networkRoomTokenDto;
-    let videoGrant;
-    if (room !== 'undefined') {
-      videoGrant = new VideoGrant({ room });
-    } else {
-      videoGrant = new VideoGrant();
-    }
-    const token: any = twilioConfig();
+    const videoGrant = room ? new VideoGrant({ room }) : new VideoGrant();
+    const token: any = this.twilioConfig;
     token.addGrant(videoGrant);
     token.identity = identity;
     return token;
   }
 
   killRoom(params: { sid: string }): Promise<string | void> {
-    return clientConfig()
-      .video.rooms(params.sid)
+    return this.clientConfig.video
+      .rooms(params.sid)
       .update({ status: 'completed' })
-      .then(room => {
-        return room.uniqueName;
-      })
-      .catch(console.log);
+      .then(room => room.uniqueName)
+      .catch(catchError);
   }
 }
--- src/shared/config/Twilio/index.ts
+++ src/shared/config/Twilio/index.ts
@@ -2,18 +2,24 @@ import * as Twilio from 'twilio';
 
 const { AccessToken } = Twilio.jwt;
 
-const clientConfig = () => {
-  const apiKeySid = process.env.TWILIO_API_KEY;
-  const apiKeySecret = process.env.TWILIO_API_SECRET;
-  const accountSid = process.env.TWILIO_ACCOUNT_SID;
+const clientConfig = ({ apiKeySid, apiKeySecret, accountSid }) => {
   return Twilio(apiKeySid, apiKeySecret, { accountSid });
 };
 
-const twilioConfig: any = () => {
-  const apiKeySid = process.env.TWILIO_API_KEY;
-  const apiKeySecret = process.env.TWILIO_API_SECRET;
-  const accountSid = process.env.TWILIO_ACCOUNT_SID;
+const twilioConfig: any = ({ apiKeySid, apiKeySecret, accountSid }) => {
   return new AccessToken(accountSid, apiKeySid, apiKeySecret);
 };
 
-export { clientConfig, twilioConfig };
+const config = () => {
+  const settings = {
+    apiKeySid: process.env.TWILIO_API_KEY,
+    apiKeySecret: process.env.TWILIO_API_SECRET,
+    accountSid: process.env.TWILIO_ACCOUNT_SID,
+  };
+  return {
+    clientConfig: clientConfig.bind(null, settings),
+    twilioConfig: twilioConfig.bind(null, settings),
+  };
+};
+
+export { config };
