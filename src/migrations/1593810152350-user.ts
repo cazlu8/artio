bc857743e237deccdb578050886e67b02a7ab648
PedroDiniz <pedro_nork@hotmail.com>
2020-07-06 16:55:41 -0300

    - migration reformat with prettier

--- src/migrations/1593810152350-user.ts
+++ src/migrations/1593810152350-user.ts
@@ -1,16 +1,19 @@
-import {MigrationInterface, QueryRunner} from "typeorm";
+import { MigrationInterface, QueryRunner } from 'typeorm';
 
 export class user1593810152350 implements MigrationInterface {
-    name = 'user1593810152350'
+  name = 'user1593810152350';
 
-    public async up(queryRunner: QueryRunner): Promise<void> {
-        await queryRunner.query(`CREATE TYPE "user_gender_enum" AS ENUM('1', '2', '3')`);
-        await queryRunner.query(`CREATE TABLE "user" ("id" SERIAL NOT NULL, "guid" character varying NOT NULL, "first_name" character varying(255), "last_name" character varying(255), "email" character varying(70) NOT NULL, "avatar_img_url" character varying(255), "bio" character varying(2000), "phone_number" character varying(50), "gender" "user_gender_enum", "company" character varying(255), "current_position" character varying(255), "social_urls" json, "is_new" boolean NOT NULL DEFAULT true, "created_at" TIMESTAMP NOT NULL DEFAULT now(), "updated_at" TIMESTAMP NOT NULL DEFAULT now(), CONSTRAINT "UQ_61ea3ae73af64f7ce8e9fe55e10" UNIQUE ("guid"), CONSTRAINT "UQ_e12875dfb3b1d92d7d7c5377e22" UNIQUE ("email"), CONSTRAINT "UQ_01eea41349b6c9275aec646eee0" UNIQUE ("phone_number"), CONSTRAINT "PK_cace4a159ff9f2512dd42373760" PRIMARY KEY ("id"))`);
-    }
+  public async up(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.query(
+      `CREATE TYPE "user_gender_enum" AS ENUM('1', '2', '3')`,
+    );
+    await queryRunner.query(
+      `CREATE TABLE "user" ("id" SERIAL NOT NULL, "guid" character varying NOT NULL, "first_name" character varying(255), "last_name" character varying(255), "email" character varying(70) NOT NULL, "avatar_img_url" character varying(255), "bio" character varying(2000), "phone_number" character varying(50), "gender" "user_gender_enum", "company" character varying(255), "current_position" character varying(255), "social_urls" json, "is_new" boolean NOT NULL DEFAULT true, "created_at" TIMESTAMP NOT NULL DEFAULT now(), "updated_at" TIMESTAMP NOT NULL DEFAULT now(), CONSTRAINT "UQ_61ea3ae73af64f7ce8e9fe55e10" UNIQUE ("guid"), CONSTRAINT "UQ_e12875dfb3b1d92d7d7c5377e22" UNIQUE ("email"), CONSTRAINT "UQ_01eea41349b6c9275aec646eee0" UNIQUE ("phone_number"), CONSTRAINT "PK_cace4a159ff9f2512dd42373760" PRIMARY KEY ("id"))`,
+    );
+  }
 
-    public async down(queryRunner: QueryRunner): Promise<void> {
-        await queryRunner.query(`DROP TABLE "user"`);
-        await queryRunner.query(`DROP TYPE "user_gender_enum"`);
-    }
-
+  public async down(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.query(`DROP TABLE "user"`);
+    await queryRunner.query(`DROP TYPE "user_gender_enum"`);
+  }
 }
--- src/migrations/1593810152850-user-events-roles.ts
+++ src/migrations/1593810152850-user-events-roles.ts
@@ -1,18 +1,27 @@
-import {MigrationInterface, QueryRunner} from "typeorm";
+import { MigrationInterface, QueryRunner } from 'typeorm';
 
 export class usereventsroles1593810152850 implements MigrationInterface {
-    name = 'usereventsroles1593810152850'
+  name = 'usereventsroles1593810152850';
 
-    public async up(queryRunner: QueryRunner): Promise<void> {
-        await queryRunner.query(`CREATE TABLE "user_events_roles" ("userEventsId" integer NOT NULL, "roleId" integer NOT NULL, "userEventsUserId" integer, "userEventsEventId" integer, CONSTRAINT "PK_565086969e12db53dba478b7897" PRIMARY KEY ("userEventsId", "roleId"))`);
-        await queryRunner.query(`ALTER TABLE "user_events_roles" ADD CONSTRAINT "FK_adaf3789688c917cf7ab2089580" FOREIGN KEY ("roleId") REFERENCES "role"("id") ON DELETE NO ACTION ON UPDATE NO ACTION`);
-        await queryRunner.query(`ALTER TABLE "user_events_roles" ADD CONSTRAINT "FK_da0c026865e245b1511a0e87710" FOREIGN KEY ("userEventsId", "userEventsUserId", "userEventsEventId") REFERENCES "user_events"("id","userId","eventId") ON DELETE NO ACTION ON UPDATE NO ACTION`);
-    }
+  public async up(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.query(
+      `CREATE TABLE "user_events_roles" ("userEventsId" integer NOT NULL, "roleId" integer NOT NULL, "userEventsUserId" integer, "userEventsEventId" integer, CONSTRAINT "PK_565086969e12db53dba478b7897" PRIMARY KEY ("userEventsId", "roleId"))`,
+    );
+    await queryRunner.query(
+      `ALTER TABLE "user_events_roles" ADD CONSTRAINT "FK_adaf3789688c917cf7ab2089580" FOREIGN KEY ("roleId") REFERENCES "role"("id") ON DELETE NO ACTION ON UPDATE NO ACTION`,
+    );
+    await queryRunner.query(
+      `ALTER TABLE "user_events_roles" ADD CONSTRAINT "FK_da0c026865e245b1511a0e87710" FOREIGN KEY ("userEventsId", "userEventsUserId", "userEventsEventId") REFERENCES "user_events"("id","userId","eventId") ON DELETE NO ACTION ON UPDATE NO ACTION`,
+    );
+  }
 
-    public async down(queryRunner: QueryRunner): Promise<void> {
-        await queryRunner.query(`ALTER TABLE "user_events_roles" DROP CONSTRAINT "FK_da0c026865e245b1511a0e87710"`);
-        await queryRunner.query(`ALTER TABLE "user_events_roles" DROP CONSTRAINT "FK_adaf3789688c917cf7ab2089580"`);
-        await queryRunner.query(`DROP TABLE "user_events_roles"`);
-    }
-
+  public async down(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.query(
+      `ALTER TABLE "user_events_roles" DROP CONSTRAINT "FK_da0c026865e245b1511a0e87710"`,
+    );
+    await queryRunner.query(
+      `ALTER TABLE "user_events_roles" DROP CONSTRAINT "FK_adaf3789688c917cf7ab2089580"`,
+    );
+    await queryRunner.query(`DROP TABLE "user_events_roles"`);
+  }
 }
