2154bee05c56668312001c56bd854b7e989ab241
PedroDiniz <pedro_nork@hotmail.com>
2020-07-13 18:15:27 -0300

    - fix guards on event routes

--- src/modules/user/user.controller.ts
+++ src/modules/user/user.controller.ts
@@ -117,7 +117,7 @@ export class UserController extends BaseWithoutAuthController {
     description: 'get events by user id',
   })
   @ApiParam({ name: 'id', type: 'number' })
-  @UseGuards(AuthGuard, VerifyIfIsAuthenticatedUserGuard)
+  @UseGuards(AuthGuard)
   @Get('events/:id')
   async getUserEvents(@Param('id', ParseIntPipe) id: number) {
     return this.userService.getEventsByUserId(id);
