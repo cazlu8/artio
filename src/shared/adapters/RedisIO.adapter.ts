f16f0fb00aa8baa3c06bd95f4a53ef805cfc20e2
cazlu8 <cazlubios8@gmail.com>
2020-07-22 16:27:53 -0300

    - fixed the env variables on redis adapter.

--- src/shared/adapters/RedisIO.adapter.ts
+++ src/shared/adapters/RedisIO.adapter.ts
@@ -2,11 +2,21 @@ import { IoAdapter } from '@nestjs/platform-socket.io';
 import * as redisIoAdapter from 'socket.io-redis';
 
 export default class RedisIoAdapter extends IoAdapter {
+  private readonly host: any;
+
+  private readonly port: number;
+
+  constructor(app: any) {
+    super(app);
+    this.host = process.env.REDIS_HOST;
+    this.port = +process.env.REDIS_PORT;
+  }
+
   createIOServer(port: number, options?: any): any {
     const server = super.createIOServer(port, options);
     const redisAdapter = redisIoAdapter({
-      host: process.env.REDIS_HOST,
-      port: +process.env.REDIS_PORT,
+      host: this.host,
+      port: this.port,
     });
     server.adapter(redisAdapter);
     return server;
