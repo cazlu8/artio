dc839507927f47fa3262893aa1017f47a95738a1
PedroDiniz <pedro_nork@hotmail.com>
2020-08-06 17:17:31 -0300

    - fix async await and remove comments

--- src/modules/event/event.service.ts
+++ src/modules/event/event.service.ts
@@ -179,7 +179,7 @@ export class EventService {
 
   async getSubscribed(eventId) {
     return await this.userEventsRepository.count({
-      where: { eventId: `${eventId}` },
+      where: { eventId },
     });
   }
 
