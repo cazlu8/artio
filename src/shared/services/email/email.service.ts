8325e41f76d90d35710ef136a5df655060f75df7
cazlu8 <cazlubios8@gmail.com>
2020-08-28 17:25:26 -0300

    roll back on send email refator to ensure that is not that not sending email from staging.

--- src/shared/services/email/email.service.ts
+++ src/shared/services/email/email.service.ts
@@ -2,25 +2,35 @@ import * as AWS from 'aws-sdk';
 import { Injectable } from '@nestjs/common';
 import { ConfigService } from '@nestjs/config';
 import SES from 'aws-sdk/clients/ses';
-import { LoggerService } from '../logger.service';
+import { SendEmailTicketCode } from '../../types/user';
 
 @Injectable()
 export class EmailService {
   private ses: SES;
 
-  constructor(
-    private configService: ConfigService,
-    private readonly loggerService: LoggerService,
-  ) {
+  constructor(private configService: ConfigService) {
     this.ses = new AWS.SES(this.configService.get('ses'));
   }
 
-  async sendTemplateBulk(params: SES.Types.SendBulkTemplatedEmailRequest) {
-    try {
-      await this.ses.sendBulkTemplatedEmail(params).promise();
-      this.loggerService.info(`email sent`);
-    } catch (error) {
-      this.loggerService.error(`error sending email:`, error);
-    }
+  async sendBulkTicketCode(data: SendEmailTicketCode) {
+    const { emails, ticketCode, eventName, eventImg, eventDate } = data;
+    const destinations = emails.map(email => ({
+      Destination: { ToAddresses: [email] },
+    }));
+    const variables = {
+      ticketCode,
+      eventName,
+      eventImg,
+      eventDate,
+      artioLogo: process.env.LOGO_EMAIL_IMG,
+      artioUrl: process.env.FRONT_END_URL,
+    };
+    const params = {
+      Destinations: destinations,
+      Source: process.env.EMAIL_NO_REPLY,
+      Template: 'sendTicketCode',
+      DefaultTemplateData: JSON.stringify(variables),
+    };
+    await this.ses.sendBulkTemplatedEmail(params).promise();
   }
 }
