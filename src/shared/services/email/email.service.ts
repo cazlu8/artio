86b185e34e91c58b9a501de2b54ada80391688c2
cazlu8 <cazlubios8@gmail.com>
2020-08-28 16:57:52 -0300

    added logs to watch email ses error.

--- src/shared/services/email/email.service.ts
+++ src/shared/services/email/email.service.ts
@@ -2,16 +2,25 @@ import * as AWS from 'aws-sdk';
 import { Injectable } from '@nestjs/common';
 import { ConfigService } from '@nestjs/config';
 import SES from 'aws-sdk/clients/ses';
+import { LoggerService } from '../logger.service';
 
 @Injectable()
 export class EmailService {
   private ses: SES;
 
-  constructor(private configService: ConfigService) {
+  constructor(
+    private configService: ConfigService,
+    private readonly loggerService: LoggerService,
+  ) {
     this.ses = new AWS.SES(this.configService.get('ses'));
   }
 
   async sendTemplateBulk(params: SES.Types.SendBulkTemplatedEmailRequest) {
-    await this.ses.sendBulkTemplatedEmail(params).promise();
+    try {
+      await this.ses.sendBulkTemplatedEmail(params).promise();
+      this.loggerService.info(`email sent`);
+    } catch (error) {
+      this.loggerService.error(`error sending email:`, error);
+    }
   }
 }
