6dbb41088b9b0d09b4fe33af93455bf59f5e28d7
cazlu8 <cazlubios8@gmail.com>
2020-09-09 18:02:24 -0300

    - fixed bug on gateway.

--- src/modules/networkRoom/networkRoom.processor.ts
+++ src/modules/networkRoom/networkRoom.processor.ts
@@ -7,7 +7,6 @@ import { Queue } from 'bull';
 import { NetworkRoomService } from './networkRoom.service';
 import { parallel } from '../../shared/utils/controlFlow.utils';
 import { UserEvents } from '../userEvents/userEvents.entity';
-import { catchError } from '../../shared/utils/errorHandler.utils';
 import { LoggerService } from '../../shared/services/logger.service';
 
 const numCPUs = require('os').cpus().length;
