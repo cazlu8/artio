3c64b4b06296ed50991e9895fcacf6f36d97380d
cazlu8 <cazlubios8@gmail.com>
2020-09-14 19:36:36 -0300

    - fixed bug on switch room.

--- src/modules/networkRoom/networkRoom.processor.ts
+++ src/modules/networkRoom/networkRoom.processor.ts
@@ -166,7 +166,7 @@ export class NetworkRoomProcessor {
             ({ currentRoom }) => currentRoom !== parsedClients[0].currentRoom,
           )
           .concat(parsedClients[0])
-          .slice(-1, -3)
+          .slice(-2)
           .map(({ socketId }) => {
             return this.service.switchRoom(eventId, socketId, room);
           });
