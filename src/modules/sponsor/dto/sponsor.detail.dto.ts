b1f55d3a9e3db93f13901e5006f4463e83bd9b9b
PedroDiniz <pedro_nork@hotmail.com>
2020-09-15 18:15:16 -0300

    - changes on get sponsors details

--- src/modules/sponsor/dto/sponsor.detail.dto.ts
+++ src/modules/sponsor/dto/sponsor.detail.dto.ts
@@ -1,4 +1,4 @@
-import { IsEnum, IsNotEmpty } from 'class-validator';
+import { IsEnum, IsNotEmpty, IsOptional } from 'class-validator';
 import { ApiProperty } from '@nestjs/swagger';
 import { SponsorTier } from '../enums/sponsor.tier.enum';
 
@@ -9,18 +9,42 @@ export class SponsorDetail {
 
   @ApiProperty()
   @IsNotEmpty()
-  banner: string;
-
-  @IsNotEmpty()
-  @IsEnum(SponsorTier)
-  @ApiProperty()
-  tier?: SponsorTier;
-
-  @ApiProperty()
-  @IsNotEmpty()
   name: string;
 
   @ApiProperty()
   @IsNotEmpty()
-  description: string;
+  banner: string;
+
+  @ApiProperty()
+  @IsOptional()
+  externalLink?: string;
+
+  @IsNotEmpty()
+  @IsEnum(SponsorTier)
+  @ApiProperty()
+  tier: SponsorTier;
+
+  @ApiProperty()
+  @IsNotEmpty()
+  email: string;
+
+  @ApiProperty()
+  @IsOptional()
+  logo?: string;
+
+  @ApiProperty()
+  @IsNotEmpty()
+  inShowRoom: string;
+
+  @ApiProperty()
+  @IsOptional()
+  description?: string;
+
+  @ApiProperty()
+  @IsOptional()
+  phoneNumber?: string;
+
+  @ApiProperty()
+  @IsOptional()
+  address?: string;
 }
