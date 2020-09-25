8439746e7df9882d5139243ba6e32bab4ebed189
Pedro Diniz <pedro_nork@hotmail.com>
2020-09-25 17:45:46 -0300

    Single sign in (#319)
    * - initial socket and endpoint structure
    
    * - tests in single login
    
    * - remove console.log
    
    * - remove console.log
    
    * - adding guid pipe
    
    * - remove eventEmmiter
    
    * - splitting endpoints
    
    * - fix on disconnect socket.
    
    * - remove log
    
    * - remove log
    
    * - fix endpoint name
    
    * - fix endpoint name

---
+++ src/modules/user/user.gateway.ts
@@ -0,0 +1,55 @@
+import {
+  BaseWsExceptionFilter,
+  OnGatewayConnection,
+  OnGatewayDisconnect,
+  WebSocketGateway,
+  WebSocketServer,
+} from '@nestjs/websockets';
+import * as bluebird from 'bluebird';
+import { UseFilters, UseGuards, UseInterceptors } from '@nestjs/common';
+import { Server } from 'socket.io';
+import { RedisService } from 'nestjs-redis';
+import { WsAuthGuard } from '../../shared/guards/wsAuth.guard';
+import { ErrorsInterceptor } from '../../shared/interceptors/errors.interceptor';
+import { JwtService } from '../../shared/services/jwt.service';
+
+@UseGuards(WsAuthGuard)
+@UseFilters(new BaseWsExceptionFilter())
+@UseInterceptors(ErrorsInterceptor)
+@WebSocketGateway(3030, { namespace: 'user', transports: ['websocket'] })
+export class UserGateway implements OnGatewayConnection, OnGatewayDisconnect {
+  @WebSocketServer()
+  readonly server: Server;
+
+  private readonly redisClient: any;
+
+  constructor(
+    private readonly jwtService: JwtService,
+    private readonly redisService: RedisService,
+  ) {
+    this.redisClient = bluebird.promisifyAll(this.redisService.getClient());
+  }
+
+  async handleDisconnect(socket: any) {
+    await this.redisClient.hdel('connectedUsers', `${socket.userId}`);
+  }
+
+  async handleConnection(socket: any) {
+    try {
+      const { token, hash } = socket.handshake.query;
+      const { sub } = await this.jwtService.validateToken(token);
+      socket.userId = sub;
+      await this.redisClient.hset(
+        'connectedUsers',
+        `${socket.userId}`,
+        `${socket.id}--${hash}`,
+      );
+    } catch (err) {
+      socket.disconnect();
+    }
+  }
+
+  async sendSignOutMessage(socketId: string) {
+    await this.server.to(socketId).emit('signOut', true);
+  }
+}
