626dbe238659a39237f74cca23a7ccda46ce9333
cazlu8 <cazlubios8@gmail.com>
2020-10-02 16:58:20 -0300

    - fixed create event.

--- src/modules/event/dto/event.create.dto.ts
+++ src/modules/event/dto/event.create.dto.ts
@@ -1,10 +1,62 @@
 import { ApiProperty } from '@nestjs/swagger';
-import { IsNotEmpty } from 'class-validator';
+import { IsNotEmpty, IsOptional } from 'class-validator';
 
 export default class CreateEventDTO {
   @ApiProperty()
   @IsNotEmpty()
   name: string;
+
+  @ApiProperty()
+  @IsOptional()
+  heroImgUrl?: string;
+
+  @ApiProperty()
+  @IsOptional()
+  locationName?: string;
+
+  @ApiProperty()
+  @IsOptional()
+  streetName: string;
+
+  @ApiProperty()
+  @IsOptional()
+  streetNumber: string;
+
+  @ApiProperty()
+  @IsOptional()
+  stateAcronym: string;
+
+  @ApiProperty()
+  @IsOptional()
+  state: string;
+
+  @ApiProperty()
+  @IsOptional()
+  country: string;
+
+  @ApiProperty()
+  @IsOptional()
+  city: string;
+
+  @ApiProperty()
+  @IsOptional()
+  zipCode: string;
+
+  @ApiProperty()
+  @IsOptional()
+  description: string;
+
+  @ApiProperty()
+  @IsOptional()
+  additionalInfo: string;
+
+  @ApiProperty()
+  @IsOptional()
+  locationLongitude?: number;
+
+  @ApiProperty()
+  @IsOptional()
+  locationLatitude?: number;
 
   @ApiProperty()
   @IsNotEmpty()
@@ -17,4 +69,20 @@ export default class CreateEventDTO {
   @ApiProperty()
   @IsNotEmpty()
   timezone: string;
+
+  @ApiProperty()
+  @IsOptional()
+  liveUrl?: string;
+
+  @ApiProperty()
+  @IsOptional()
+  streamKey?: string;
+
+  @ApiProperty()
+  @IsOptional()
+  streamUrl?: string;
+
+  @ApiProperty()
+  @IsOptional()
+  onLive?: boolean;
 }
--- src/modules/event/event.subscriber.ts
+++ src/modules/event/event.subscriber.ts
@@ -7,14 +7,10 @@ import { uuid } from 'uuidv4';
 import { InjectQueue } from '@nestjs/bull';
 import { Queue } from 'bull';
 import { Event } from './event.entity';
-import { EventService } from './event.service';
 
 @Subscriber()
 export class EventSubscriber implements EntitySubscriberInterface<Event> {
-  constructor(
-    private service: EventService,
-    @InjectQueue('event') private readonly eventQueue: Queue,
-  ) {}
+  constructor(@InjectQueue('event') private readonly eventQueue: Queue) {}
 
   listenTo() {
     return Event;
@@ -22,10 +18,5 @@ export class EventSubscriber implements EntitySubscriberInterface<Event> {
 
   beforeInsert(event: InsertEvent<Event>) {
     event.entity.guid = uuid();
-  }
-
-  async afterInsert(event: InsertEvent<Event>) {
-    const { id } = event.entity;
-    await this.service.addDestroyInfraToQueue(id);
   }
 }
