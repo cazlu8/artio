a1ab5fe5b9cad206ad71f884d680509827160155
cazlu8 <cazlubios8@gmail.com>
2020-10-02 17:02:03 -0300

    - fixed create event.

--- src/modules/event/event.subscriber.ts
+++ src/modules/event/event.subscriber.ts
@@ -4,14 +4,10 @@ import {
   EventSubscriber as Subscriber,
 } from 'typeorm';
 import { uuid } from 'uuidv4';
-import { InjectQueue } from '@nestjs/bull';
-import { Queue } from 'bull';
 import { Event } from './event.entity';
 
 @Subscriber()
 export class EventSubscriber implements EntitySubscriberInterface<Event> {
-  constructor(@InjectQueue('event') private readonly eventQueue: Queue) {}
-
   listenTo() {
     return Event;
   }
