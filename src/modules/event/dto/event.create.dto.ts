5d1f8ee863cd39c817715760a5378df5afdd0fbd
cazlu8 <cazlubios8@gmail.com>
2020-10-02 16:44:46 -0300

    - fixed create event dto to accept only the right fields.

--- src/modules/event/dto/event.create.dto.ts
+++ src/modules/event/dto/event.create.dto.ts
@@ -1,62 +1,10 @@
 import { ApiProperty } from '@nestjs/swagger';
-import { IsNotEmpty, IsOptional } from 'class-validator';
+import { IsNotEmpty } from 'class-validator';
 
 export default class CreateEventDTO {
   @ApiProperty()
   @IsNotEmpty()
   name: string;
-
-  @ApiProperty()
-  @IsOptional()
-  heroImgUrl?: string;
-
-  @ApiProperty()
-  @IsOptional()
-  locationName?: string;
-
-  @ApiProperty()
-  @IsOptional()
-  streetName: string;
-
-  @ApiProperty()
-  @IsOptional()
-  streetNumber: string;
-
-  @ApiProperty()
-  @IsOptional()
-  stateAcronym: string;
-
-  @ApiProperty()
-  @IsOptional()
-  state: string;
-
-  @ApiProperty()
-  @IsOptional()
-  country: string;
-
-  @ApiProperty()
-  @IsOptional()
-  city: string;
-
-  @ApiProperty()
-  @IsOptional()
-  zipCode: string;
-
-  @ApiProperty()
-  @IsOptional()
-  description: string;
-
-  @ApiProperty()
-  @IsOptional()
-  additionalInfo: string;
-
-  @ApiProperty()
-  @IsOptional()
-  locationLongitude?: number;
-
-  @ApiProperty()
-  @IsOptional()
-  locationLatitude?: number;
 
   @ApiProperty()
   @IsNotEmpty()
@@ -69,20 +17,4 @@ export default class CreateEventDTO {
   @ApiProperty()
   @IsNotEmpty()
   timezone: string;
-
-  @ApiProperty()
-  @IsOptional()
-  liveUrl?: string;
-
-  @ApiProperty()
-  @IsOptional()
-  streamKey?: string;
-
-  @ApiProperty()
-  @IsOptional()
-  streamUrl?: string;
-
-  @ApiProperty()
-  @IsOptional()
-  onLive?: boolean;
 }
