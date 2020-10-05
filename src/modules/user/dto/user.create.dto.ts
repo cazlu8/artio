aa064b00b0a79f5ff9d26361da469a85cca10a9f
PedroDiniz <pedro_nork@hotmail.com>
2020-10-05 15:44:51 -0300

    - changes on create user dto.

--- src/modules/user/dto/user.create.dto.ts
+++ src/modules/user/dto/user.create.dto.ts
@@ -1,5 +1,12 @@
-import { IsEmail, IsNotEmpty } from 'class-validator';
+import {
+  IsEmail,
+  IsEnum,
+  IsNotEmpty,
+  IsOptional,
+  IsPhoneNumber,
+} from 'class-validator';
 import { ApiProperty } from '@nestjs/swagger';
+import { UserGender } from '../enums/user.gender.enum';
 
 export class CreateUserDto {
   @IsNotEmpty()
@@ -10,4 +17,38 @@ export class CreateUserDto {
   @ApiProperty()
   @IsNotEmpty()
   email: string;
+
+  @ApiProperty()
+  @IsOptional()
+  firstName?: string;
+
+  @ApiProperty()
+  @IsOptional()
+  lastName?: string;
+
+  @ApiProperty()
+  @IsOptional()
+  bio?: string;
+
+  @IsOptional()
+  @IsPhoneNumber('ZZ')
+  @ApiProperty()
+  phoneNumber?: string;
+
+  @IsOptional()
+  @IsEnum(UserGender)
+  @ApiProperty()
+  gender?: UserGender;
+
+  @ApiProperty()
+  @IsOptional()
+  company?: string;
+
+  @ApiProperty()
+  @IsOptional()
+  currentPosition?: string;
+
+  @IsOptional()
+  @ApiProperty()
+  socialUrls?: string;
 }
