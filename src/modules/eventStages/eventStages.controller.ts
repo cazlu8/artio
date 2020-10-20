89b5b13849f03bc00f478d56e6694631e7a7737a
cazlu8 <cazlubios8@gmail.com>
2020-10-20 17:09:51 -0300

    - fix on event stages.

--- src/modules/eventStages/eventStages.controller.ts
+++ src/modules/eventStages/eventStages.controller.ts
@@ -1,10 +1,11 @@
-import { Body, Controller, Post } from '@nestjs/common';
+import { Body, Controller, Post, UsePipes } from '@nestjs/common';
 import { ApiTags, ApiCreatedResponse } from '@nestjs/swagger';
 import { ObjectLiteral } from 'typeorm';
 import { BaseController } from '../../shared/controllers/base.controller';
 import { EventStagesRepository } from './eventStages.repository';
 import CreateEventStageDTO from './dto/eventStage.create.dto';
 import { LoggerService } from '../../shared/services/logger.service';
+import { ValidateIfEventExists } from '../event/pipes/ValidateIfEventExists.pipe';
 
 @ApiTags('EventStage')
 @Controller('eventStage')
@@ -21,6 +22,7 @@ export class EventStagesController extends BaseController {
     description: 'The event stage has been successfully created',
   })
   @Post()
+  @UsePipes(ValidateIfEventExists)
   async create(
     @Body() createEventDto: CreateEventStageDTO,
   ): Promise<void | ObjectLiteral> {
