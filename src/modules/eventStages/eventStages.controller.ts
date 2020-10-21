54d3283bffd92a201ffee1c0b8c459bde4e5ceda
Lucas Reichert Siqueira <cazlubios8@gmail.com>
2020-10-21 18:01:04 -0300

    - reverse order on get messages. (#384)

--- src/modules/eventStages/eventStages.controller.ts
+++ src/modules/eventStages/eventStages.controller.ts
@@ -1,4 +1,4 @@
-import { Body, Controller, Post, UsePipes } from '@nestjs/common';
+import { Body, Controller, Get, Param, Post, UsePipes } from '@nestjs/common';
 import { ApiTags, ApiCreatedResponse } from '@nestjs/swagger';
 import { ObjectLiteral } from 'typeorm';
 import { BaseController } from '../../shared/controllers/base.controller';
@@ -6,6 +6,7 @@ import { EventStagesRepository } from './eventStages.repository';
 import CreateEventStageDTO from './dto/eventStage.create.dto';
 import { LoggerService } from '../../shared/services/logger.service';
 import { ValidateIfEventExists } from '../event/pipes/ValidateIfEventExists.pipe';
+import { EventStages } from './eventStages.entity';
 
 @ApiTags('EventStage')
 @Controller('eventStage')
@@ -31,5 +32,14 @@ export class EventStagesController extends BaseController {
       `Event ${eventStage.name} created for the event ${eventStage.eventId}`,
     );
     return eventStage;
+  }
+
+  @ApiCreatedResponse({
+    description: 'get stages by eventId',
+  })
+  @Get('/:eventId')
+  @UsePipes(ValidateIfEventExists)
+  async get(@Param() eventId: number): Promise<void | EventStages[]> {
+    return this.repository.getStagesByEventId(eventId);
   }
 }
