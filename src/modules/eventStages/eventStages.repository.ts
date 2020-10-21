77b2034a793d9307037a14190bbdede39c74a5f4
cazlu8 <cazlubios8@gmail.com>
2020-10-21 17:58:42 -0300

    - reverse order on get messages.

--- src/modules/eventStages/eventStages.repository.ts
+++ src/modules/eventStages/eventStages.repository.ts
@@ -2,4 +2,11 @@ import { EntityRepository, Repository } from 'typeorm';
 import { EventStages } from './eventStages.entity';
 
 @EntityRepository(EventStages)
-export class EventStagesRepository extends Repository<EventStages> {}
+export class EventStagesRepository extends Repository<EventStages> {
+  async getStagesByEventId(eventId: number) {
+    return this.createQueryBuilder('eventStages')
+      .select(['id', 'name'])
+      .where('eventStages.eventId = :eventId', { eventId })
+      .getRawMany();
+  }
+}
