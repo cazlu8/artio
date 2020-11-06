24ef3f99c1be0b0ef1810e5bee0fe6277295988b
cazlu8 <cazlubios8@gmail.com>
2020-11-06 17:28:14 -0300

    eslint fix.

--- src/modules/speaker/dto/speaker.create.dto.ts
+++ src/modules/speaker/dto/speaker.create.dto.ts
@@ -1,10 +1,4 @@
-import {
-  IsEmail,
-  IsNotEmpty,
-  IsObject,
-  IsOptional,
-  IsUrl,
-} from 'class-validator';
+import { IsEmail, IsNotEmpty, IsOptional, IsUrl } from 'class-validator';
 import { ApiProperty } from '@nestjs/swagger';
 
 export class SpeakerCreateDTO {
